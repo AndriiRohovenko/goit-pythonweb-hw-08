fd6033736d7296277ffd61248d8e3931e10d413d
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-09-07 17:21:54 +0300

    implement Users model and migrated to the db

--- src/db/models.py
+++ src/db/models.py
@@ -1,7 +1,18 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, DateTime
+from sqlalchemy import ForeignKey, DateTime, String
 from datetime import datetime
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    surname: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    birthdate: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    additional_info: Mapped[str | None] = mapped_column(String(255), nullable=True)
